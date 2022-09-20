016505b9d4e2004266fc76a61938fb3c9ceab331
hyerriimm <gwj06129@naver.com>
2022-09-20 17:02:24 +0900

    레이아웃 설정

--- src/pages/login/LoginPg.jsx
+++ src/pages/login/LoginPg.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Header from '../../components/Header';
+import Layout from '../../components/Layout';
+import Login from './component/Login';
 
 const LoginPg = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <>
+      <Header />
+      <Layout>
+        <Login/>
+      </Layout>
+    </>
+  );
+};
 
 export default LoginPg;
