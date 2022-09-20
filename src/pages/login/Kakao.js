461bc4386bd5f81f51c483787d9043326f4e6574
hyerriimm <gwj06129@naver.com>
2022-09-20 17:02:58 +0900

    카카오 로그인 구현중

--- src/pages/login/Kakao.js
+++ src/pages/login/Kakao.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Kakao = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const navigate = useNavigate();
+  let code = new URL(window.location.href).searchParams.get('code');
+  console.log(code);
+  React.useEffect(() => {
+    kakaologin(code);
+  }, []);
+
+  const kakaologin = async (_code) => {
+    try {
+      const response = await axios.get(`api/member/kakao?code=${_code}`); //백엔드로 인가코드 보내기
+
+      localStorage.setItem('ACCESS_TOKEN', response.headers.authorization);
+      localStorage.setItem('REFRESHTOKEN', response.headers.refreshtoken);
+      console.log(response);
+
+      if(response.data.success === true) {
+        alert('로그인 되었습니다.');
+        navigate('/');
+      }
+    } catch (error) {
+      console.lof(error);  
+      console.log('카카오 로그인 실패');
+    }
+  };
+
+  return null;
+};
 
 export default Kakao;
