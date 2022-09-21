9f0dd8255f2a8968d87946b1f9c54c71144244aa
pisipp <pisipp24@gmail.com>
2022-09-21 18:43:46 +0900

    슬라이더 배너 생성

--- src/pages/main/MainPg.jsx
+++ src/pages/main/MainPg.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Banner from "./component/Banner"
 
 const MainPg = () => {
     return (
         <div>
             <Header/>
+            <Banner/>
+            
         </div>
     );
 }
