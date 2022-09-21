e9a38c08ece6af87a7ae543531a6270812280ef0
pisipp <pisipp24@gmail.com>
2022-09-21 22:30:59 +0900

    메인보드 생성

--- src/pages/main/MainPg.jsx
+++ src/pages/main/MainPg.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Banner from "./component/Banner"
+import MainBoard from './component/MainBoard';
 
 const MainPg = () => {
     return (
         <div>
             <Header/>
             <Banner/>
-            
+            <MainBoard/>
         </div>
     );
 }
