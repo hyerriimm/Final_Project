5f83eebcb8ccb88d2eb803a446f4b540c94f26de
pisipp <pisipp24@gmail.com>
2022-09-21 12:20:48 +0900

    모달 구현 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
 import { React, useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-// import Dropdown from '../components/Dropdown'
-// import useDetectClose from "../hooks/useDetectClose";
+
 
 const Header = () => {
     const navigate = useNavigate();
     const [accesstoken,setAccesstoken] = useState(undefined);
     const [profileImg,setProfileImg] = useState(undefined);
-    // const [isOpen, setIsOpen, ref, removeHandler] = useDetectClose(false);
-
     const profile = `${profileImg}` // "https://avatars.dicebear.com/api/adventurer-neutral/:seed.svg"
+
+    // const modalRef = useRef();
+    const [isOpen, setIsOpen] = useState(false);
+    const handleModal = () => {
+      setIsOpen(!isOpen)
+    };
+
 
     useEffect(()=>{
       const timeout = setTimeout(()=>{
@@ -24,22 +28,42 @@ const Header = () => {
       setProfileImg(localStorage.getItem("ImgURL"));
     });
 
-    
+
+    // useEffect(() => {
+    //   const onClickOutside = (e) => {
+    //       if (elem.current !== null && !elem.current.contains(e.target)) {
+    //           setIsOpen(!isOpen);
+    //       }
+    //   };
+    //   if (isOpen) {
+    //       window.addEventListener("click", onClickOutside);
+    //   }
+    //   return () => {
+    //       window.removeEventListener("click", onClickOutside);
+    //   };
+    // });
+
+    const siteLogout = () => {
+      if (window.confirm("로그아웃 하시겠습니까?")) {
+        setAccesstoken(localStorage.removeItem("ACCESSTOKEN"));
+        setAccesstoken(localStorage.removeItem("REFRESHTOKEN"));
+        setProfileImg(localStorage.removeItem("ImgURL"));
+        alert('로그아웃 되었습니다.')
+        navigate("/");
+      } else {
+          console.log("로그인 유지");
+      }
+  };
     
     return (
         <>
         <HdContainer>
             <Logo onClick={()=>navigate('/')}>3355</Logo>
             <BtnWrapper>            
-                    {/* <Btn onClick={()=>navigate('/form')}>모임등록</Btn>
-                    <BtnProfile>
-                      <img src={ profile } alt="profile"/>
-                    </BtnProfile>
-                    <Btn onClick={()=>navigate('/login')}>로그인</Btn> */}
                 { accesstoken ? 
                 ( <>
                   <Btn onClick={()=>navigate('/form')}>모임등록</Btn>
-                  <BtnProfile>
+                  <BtnProfile onClick={handleModal}>
                       <img src={ profile } alt="profile"/>
                   </BtnProfile>
                   </>
@@ -49,10 +73,14 @@ const Header = () => {
                 )}
             </BtnWrapper>
         </HdContainer>
+        {isOpen === false ? null 
+        : <ModalBackdrop onClick={handleModal}>
         <Menu>
-          <div onClick={()=>navigate('/mypage')}>마이페이지</div>
-          <div>로그아웃</div>
+          <MenuText onClick={()=>navigate('/mypage')}>마이페이지</MenuText>
+          <MenuText onClick={siteLogout}>로그아웃</MenuText>
         </Menu>
+        </ModalBackdrop>
+        }
         </>
     );
 };
@@ -107,6 +135,11 @@ const BtnProfile = styled.div`
           border-radius: 100%;          
       }
 `
+
+const ModalBackdrop = styled.div`
+    
+`
+
 const Menu = styled.div`
     background: #fff;
     border-radius: 8px;
@@ -114,8 +147,14 @@ const Menu = styled.div`
     top: 50px;
     right: 25px;
     width: 150px;
-    padding: 10px;
+    padding: 20px 0 10px 0;
     text-align: center;
     box-shadow: 0 1px 8px rgba(0, 0, 0, 0.3);
     z-index: 1000;
+`
+
+const MenuText = styled.div`
+    margin: 0 0 10px 0;
+    font-size: 14px;
+    cursor: pointer;
 `
